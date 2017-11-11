f7cf04e62d399865dc1c1d0d05636ee5c0716419
Ehsani <Ehsani@BUSINESSDEV2>
2017-11-11 13:42:54 +0330

    Third Commit

--- app/src/main/java/com/example/kahkeshaniha/androidsoundrecorder/MainActivity.java
+++ app/src/main/java/com/example/kahkeshaniha/androidsoundrecorder/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Start Project , Second Commit
+        //Start Project , Third Commit
+        //Some Changes
     }
 }
